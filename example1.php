7dbf6806a60c551a60aed76728465bfc120bef3e
Haezal Musa <ezalepy@gmail.com>
2015-03-24 00:38:03 +0800

    add example1.

---
+++ example1.php
@@ -0,0 +1,23 @@
+<?php  
+/**
+ * Contoh class, property dan method
+ *
+ * @package default
+ * @author haezal musa
+ **/
+class NamaClass
+{
+	public $variable; // properti
+
+	/**
+	 * Method
+	 *
+	 * @return void
+	 * @author haezal musa
+	 **/
+	function namaFungsi()
+	{
+		// isi fungsi / method
+	}
+} // END class NamaClass
+?>
