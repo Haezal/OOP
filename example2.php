35f595419064e5190083906ef856e54c8208df91
Haezal Musa <ezalepy@gmail.com>
2015-03-24 20:38:47 +0800

    add comment

--- example2.php
+++ example2.php
@@ -1,7 +1,7 @@
 <?php  
 include "constructor.php";
 
-$kereta = new Kereta("perodua", "myvi", "oren");
+$kereta = new Kereta("perodua", "myvi", "oren"); // give default value to class properti using constructor
 
 echo "<h3>Kereta pertama</h3>";
 echo "Jenama : ".$kereta->jenama."<br>";
@@ -12,8 +12,8 @@ echo "Mendapat diskaun : ".$kereta->diskaun();
 $kereta2 = new Kereta("proton", "saga", "putih");
 
 echo "<h3>Kereta pertama</h3>";
-echo "Jenama : ".$kereta2->jenama."<br>";
-echo "Model : ".$kereta2->model."<br>";
+echo "Jenama : ".$kereta2->jenama."<br>"; // call properti
+echo "Model : ".$kereta2->model."<br>"; // call properti
 echo "Warna : ".$kereta2->warna."<br>";
-echo "Mendapat diskaun : ".$kereta2->diskaun();
+echo "Mendapat diskaun : ".$kereta2->diskaun(); // call method
 ?>
