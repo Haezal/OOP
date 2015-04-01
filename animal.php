88fe20279590d980a6609592961eefc6bef9c68a
Haezal Musa <ezalepy@gmail.com>
2015-04-01 15:59:10 +0800

    add file

---
+++ animal.php
@@ -0,0 +1,30 @@
+<?php  
+class Animal{
+ 
+    private $type = "animal";
+ 	protected $type2 = "animal";
+
+    public function private_dance() {
+        echo $this->type . " dances!\n";
+        return true;
+    }
+
+    public function protected_dance(){
+    	echo $this->type2 . " dances!\n";
+        return true;
+    }
+
+}
+
+class Penguin extends Animal {
+    public $type = "penguin";
+ 	public $type2 = "penguin";
+}
+
+$tux = new Penguin();
+ 
+// make tux dance
+$tux->private_dance();
+echo "<br>";
+$tux->protected_dance();
+?>
